5beeb81ec76d763c891999de8f7934201e0cfd07
Cory Trimm <cory.trimm@gmail.com>
2025-03-15 21:33:31 -0300

    switch to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import { AuditProvider } from './context/AuditContext.tsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: "/gh-pages-audit-app/",
+    path: "/",
     element: <App />,
   },
-], {
-  basename: "/gh-pages-audit-app"
-});
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
